a9fb6a080ac3c2010c120b3cef80bc2615351e11
gbaiki <47975542+gbaiki@users.noreply.github.com>
2019-04-01 13:36:40 +0100

    synonym page validated
    fdsfsd

--- js/words.js
+++ js/words.js
@@ -2,51 +2,80 @@ $(document).ready(function () {
 
     function getTargets(apiFn, sourceWord, max, response) {
         $.ajax({
-                dataType: "json",
-                type : 'Get',
-                url: '//api.datamuse.com/words?' + apiFn + '=' + sourceWord + '&max=' + max,
-                success: function(data) {
-                    response(sourceWord, $.map( data, function(item) { return item["word"]; }), apiFn);
-                },
-            });
+            dataType: "json",
+            type: 'Get',
+            url: '//api.datamuse.com/words?' + apiFn + '=' + sourceWord + '&max=' + max,
+            success: function (data) {
+                if (sourceWord == "") {
+                    alert("Please enter a word.");
+                } 
+                else if( sourceWord.includes(" ")) {
+                    alert("Cannot contain a space - please try again.");
+                }
+                else {
+                        response(sourceWord, $.map(data, function (item) {
+                            return item["word"];
+                        }), apiFn);
+                }
+            },
+        });
     }
-    
-    
+
+
     var link;
     var node;
     var rect_width = 120;
-    var width = 1000, height = 500;
+    var width = 1000,
+        height = 500;
     var svg = d3.select("#graph").append("svg")
-       .attr("width", "100%")
-       .attr("height", "100%")
-       .attr("viewBox", "0 0 960 500")
-       .attr("preserveAspectRatio", "xMidYMid meet");
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", "0 0 960 500")
+        .attr("preserveAspectRatio", "xMidYMid meet");
     var nodes = {};
-    
-    
+
+
     function response(sourceWord, followers, apiFn) {
-    
+
+        if (followers.length == 0) {
+            alert("Word doesn't exist - please try again.");
+            n["name"] = "error";
+        }
+
+
         for (var n in nodes) {
             if (n != sourceWord) {
                 delete nodes[n];
             }
         }
         svg.selectAll(".link").remove();
-        svg.selectAll(".node").filter(function (n) {return n["name"] != sourceWord}).remove();
-        svg.selectAll(".node").filter(function (n) {return n["name"] === sourceWord})
+        svg.selectAll(".node").filter(function (n) {
+            return n["name"] != sourceWord
+        }).remove();
+        svg.selectAll(".node").filter(function (n) {
+                return n["name"] === sourceWord
+            })
             .attr("class", "triggernode");
         svg.selectAll(".triggernode").remove();
         links = [];
         for (var i = 0; i < followers.length; i++) {
-            links.push({source: sourceWord, target: followers[i], type: "rel_jjv"});
+            links.push({
+                source: sourceWord,
+                target: followers[i],
+                type: "rel_jjv"
+            });
         }
 
 
-        links.forEach(function(link) {
-                link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-                link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+        links.forEach(function (link) {
+            link.source = nodes[link.source] || (nodes[link.source] = {
+                name: link.source
             });
-    
+            link.target = nodes[link.target] || (nodes[link.target] = {
+                name: link.target
+            });
+        });
+
         var force = d3.layout.force()
             .nodes(d3.values(nodes))
             .links(links)
@@ -55,55 +84,75 @@ $(document).ready(function () {
             .charge(-800)
             .on("tick", tick)
             .start();
-    
+
         link = svg.selectAll(".link")
             .data(force.links())
             .enter().append("line")
             .attr("class", "link");
-    
+
         node = svg.selectAll(".node")
             .data(force.nodes())
             .enter().append("g")
-            .attr("class", function(d,i) {if (d["name"]==sourceWord) return "triggernode"; else return "node";})
+            .attr("class", function (d, i) {
+                if (d["name"] == sourceWord) return "triggernode";
+                else return "node";
+            })
             .call(force.drag)
-            .on("click", function(d,i) {click(d,i,sourceWord,apiFn);});
-    
+            .on("click", function (d, i) {
+                click(d, i, sourceWord, apiFn);
+            });
+
         node.append("rect")
             .attr("width", rect_width)
             .attr("height", 30)
             .attr("rx", 50)
             .attr("ry", 15);
-    
+
         node.append("text")
-            .attr("x", rect_width / 2 )
+            .attr("x", rect_width / 2)
             .attr("dy", "1.4em")
-            .text(function(d) { return d.name; });
+            .text(function (d) {
+                return d.name;
+            });
     }
-    
+
     function tick() {
         link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-        
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
         node
-            .attr("transform", function(d) { return "translate(" + (d.x-rect_width/2) + "," + d.y + ")"; });
+            .attr("transform", function (d) {
+                return "translate(" + (d.x - rect_width / 2) + "," + d.y + ")";
+            });
     }
-    
-    
+
+
     function click(d, i, sourceWord, apiFn) {
-        if (d["name"] == sourceWord) {
+        if (d["name"] === sourceWord) {
             var node = d3.select(".triggernode")
                 .attr("class", "selectedtrigger triggernode");
             node.select("text").remove();
             node.on("click", null);
-            node.filter(function (n) {return n["name"] == sourceWord})
+            node.filter(function (n) {
+                    return n["name"] == sourceWord;
+                })
                 .append("foreignObject")
                 .attr("x", "15")
-                .append("xhtml:input").attr("id","sourceinput").attr("type","text").attr("value", sourceWord)
-                .on("keypress", function(d,i) {
-                        if (event.keyCode == 13) getTargets("rel_syn", $("#sourceinput").val(), 10, response);});
+                .append("xhtml:input").attr("id", "sourceinput").attr("type", "text").attr("value", sourceWord)
+                .on("keypress", function (d, i) {
+                    if (event.keyCode == 13) getTargets("rel_syn", $("#sourceinput").val(), 10, response);
+                });
         } else {
             var nextApiFn;
             if (apiFn == "rel_syn") {
@@ -113,7 +162,8 @@ $(document).ready(function () {
             }
             getTargets(nextApiFn, d["name"], 15, response);
         }
+
     }
-    
+
     getTargets("rel_syn", "start", 15, response);
 });
